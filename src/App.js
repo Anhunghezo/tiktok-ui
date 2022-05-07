9853d14631e12ef26caf19705cf8915c937f3e56
Kieu Huynh <Anhkieu303252@gmail.com>
2022-05-07 10:42:56 +0700

    Customize-cra, babel-plugin-module-resolver, prettier, scss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment } from 'react'
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes';
 import { DefaultLayout } from './components/GlobalStyles/Layout';
@@ -6,21 +6,31 @@ import { DefaultLayout } from './components/GlobalStyles/Layout';
 function App() {
   return (
     <Router>
-      <div className="App"> 
+      <div className="App">
         <Routes>
-         {publicRoutes.map((route, index) => {
-           const Page = route.component;
+          {publicRoutes.map((route, index) => {
+            const Page = route.component;
 
-           let Layout = DefaultLayout
+            let Layout = DefaultLayout;
 
-           if (route.layout ) {
-             Layout = route.layout
-           } else if (route.layout === null) {
-             Layout = Fragment
-           }
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
 
-           return <Route key={index} path={route.path} element={<Layout><Page/></Layout>} />;
-         })}
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
         </Routes>
       </div>
     </Router>
